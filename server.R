7e7a77e7403d09c730c1761581f16109de1d9b88
Autumn949 <alexvamason2004@gmail.com>
2022-10-11 14:37:35 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1003,7 +1003,7 @@ server <- function(input, output, session) {
   gameslist <<- reactiveValues()
   config <- fromJSON(file = "config.json")
   # database connection intialized globally
-  con <<- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = config$server, database = config$database, user = config$user, password = config$password)
+  con <<- dbConnect(odbc::odbc(), Driver= "ODBC Driver 18 for SQL Server",Server=config$server, database=config$database, uid=config$user, pwd=config$password,TrustServerCertificate="yes")
   metadata <<- dbReadTable(con, "METADATA")
   gamesdata <<- dbReadTable(con, "MATCHINFO")
   output$selectgames <- renderUI({
@@ -1011,9 +1011,9 @@ server <- function(input, output, session) {
     checkboxGroupInput("gamescheckbox", "Select Games To Load", choices = choice, selected = choice[1])
   })
   updateflagmapcharts <<- 0
-  sitenames <<- readxl::read_excel("Ref/sitenames.xlsx")
-  mapnames <<- read.csv("Ref/maps.csv")
-  opnames <<- read.csv("Ref/opnames.csv")
+  sitenames <<- readxl::read_excel("ref/sitenames.xlsx")
+  mapnames <<- read.csv("ref/maps.csv")
+  opnames <<- read.csv("ref/opnames.csv")
   
   updateSelectizeInput(session, "filterbanonedropdown", choices = append(opnames$DEF,"No Ban"), selected = "No Ban")
   updateSelectizeInput(session, "filterbantwodropdown", choices = append(opnames$DEF,"No Ban"), selected = "No Ban")
